a18d65893f263b61c34b1fe92e7f43c3443951af
ummehafsa21 <ummehafsa2197@gmail.com>
2020-10-28 02:21:47 +0600

    first commit for this project

---
+++ footer.php
@@ -0,0 +1,47 @@
+<?php include 'link.php'; ?>
+
+<!--Footer area-->
+
+<div class="footer-area pt-4">
+        <div class="container">
+          <div class="row">
+            <div class="col-3"><img src="image/logo.png" style="height: 50px;" alt="">
+              <p class="footer-para pt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, iusto.</p>
+            </div>
+            <div class="col-3">
+              <div class="row footer-heading" >Home</div> <br>
+              <div class="row "><span class="footer-heading">Products</span> <br><p class="footer-para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, iusto.</p>
+              </div>
+              <br>
+              <div class="row "><span class="footer-heading">Services</span> <br><p class="footer-para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, iusto.</p>
+              </div>
+            </div>
+            <div class="col-5">
+              <div class="row">
+                  <div class="subscribe-box">
+                      <h5>Subscribe to our Newsletter</h5>
+                          <input style="height: 30px;  font-size: 15px;" type="email" class="form-control"  aria-describedby="emailHelp" placeholder="Email"> 
+                          <button style="height:30px;   font-size: 15px;" type="submit" class=" subscribe-button btn  mt-2 p-1">Subscribe</button>
+                  </div>
+              </div>
+              <div class="row " style="color: white;">
+           
+                <div class="col-9 text-right">
+                    <a href="https://www.facebook.com/Web-Amex-115938159838216/" target="_blank"> <i class="fab fa-facebook-f footer-icon "></i></a>
+                    <a href=""> <i class="fab fa-twitter footer-icon"> </i></a>
+                 <a href=""><i class="fab fa-instagram footer-icon"> </i></a>
+                   <a href="">  <i class="fab fa-linkedin footer-icon"> </i></a>
+                </div>
+                
+           
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="boundary"></div>
+      <div class="footer">
+        <p class="text-center pt-2">webamex All rights reserved.</p>
+      </div>
+      <!--end of footer area-->
